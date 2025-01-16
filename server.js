e927a42b0948346b9a257e0d8f2ee00e2b826461
Chris Aliyuda <chrisaliyuda@gmail.com>
2025-01-17 03:24:17 +0500

    test

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => res.send('hope doing well'))
-
-var server = app.listen(process.env.PORT || 4000, () => {
-    console.log("Listening on port " + server.address().port + "...");
-});
